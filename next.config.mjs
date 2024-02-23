01e8f46992d0c1379cae5fa957081c00fef83cae
Agusioma <terryterrence200@gmail.com>
2024-02-23 09:03:39 +0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+//const nextConfig = {};
 
+//export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    'output': 'export'
+}
 export default nextConfig;
+//module.exports = nextConfig
